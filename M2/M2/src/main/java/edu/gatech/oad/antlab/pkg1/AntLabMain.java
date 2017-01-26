7929536c460bc5e4cd658b44ed8f963018585103
Shaurye <saggarwal46@gatech.edu>
2017-01-26 12:37:14 -0500

    Person3 Added Name

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -65,7 +65,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("saggarwal46");
+		  toPrint += p3.toString("saggarwasdl46");
           //Person4 replace P4 with your name
           //and gburdell4 with your gt id
           Person4 p4 = new Person4("P4");
