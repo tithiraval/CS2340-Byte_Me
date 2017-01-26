4b476b4f2930e95918821140a0a5fae25b862392
anmollal <alal30@gatech.edu>
2017-01-26 12:37:36 -0500

    changed p5

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Anmol");
+                  toPrint += p5.toString("alal30");
                   Person6 p6 = new Person6("Tithi");
                   toPrint += p6.toString("traval6");
 		  
