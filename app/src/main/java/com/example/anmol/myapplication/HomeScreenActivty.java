de82ad4ac00f8ee7bd07c97fd6270b5cdf2142cf
anmollal <alal30@gatech.edu>
2017-02-16 11:57:46 -0500

    Push from android studio

--- app/src/main/java/com/example/anmol/myapplication/HomeScreenActivty.java
+++ app/src/main/java/com/example/anmol/myapplication/HomeScreenActivty.java
@@ -5,16 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeScreenActivty extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_screen_activty);
     }
 
-    public void login(View view) {
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+    public void logOut(View view) {
+        Intent intent = new Intent(HomeScreenActivty.this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
--- app/src/main/java/com/example/anmol/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/anmol/myapplication/LoginActivity.java
@@ -1,8 +1,12 @@
 package com.example.anmol.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,5 +18,20 @@ public class LoginActivity extends AppCompatActivity {
 
     public void cancel(View view) {
         this.finish();
+    }
+
+    public void loginApp(View view) {
+        EditText username = (EditText) findViewById(R.id.enterUsername);
+        EditText password = (EditText) findViewById(R.id.enterPassword);
+
+        if (username.getText().toString().equals("user") && password.getText().toString().equals("pass")) {
+            Intent intent = new Intent(LoginActivity.this, HomeScreenActivty.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        } else {
+            CharSequence text = "Incorrect Credentials!";
+            Toast wrongPassword = Toast.makeText(LoginActivity.this.getApplicationContext(), text, Toast.LENGTH_LONG);
+            wrongPassword.show();
+        }
     }
 }
