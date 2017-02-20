a0ce1b579f80fa5f483343333aed178b5bb7e308
wiqasnassar <wiqasnassar@gmail.com>
2017-02-20 18:34:22 -0500

    Added spinner and account type

---
+++ ThirstQuencherIOS/ThirstQuencher/RegistrationViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RegistrationViewController.swift
+//  ThirstQuencher
+//
+//  Created by Wiqas Nassar on 2/20/17.
+//  Copyright © 2017 Hunter. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var pass: UITextField!
+    @IBOutlet weak var confirmPass: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
