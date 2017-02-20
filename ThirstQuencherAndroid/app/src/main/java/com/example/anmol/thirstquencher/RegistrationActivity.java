9944b0dd17a40827ba30d54c7baf6f0f53f16ff4
anmollal <alal30@gatech.edu>
2017-02-20 18:29:43 -0500

    Registration with no saving completed

--- ThirstQuencherAndroid/app/src/main/java/com/example/anmol/thirstquencher/RegistrationActivity.java
+++ ThirstQuencherAndroid/app/src/main/java/com/example/anmol/thirstquencher/RegistrationActivity.java
@@ -1,8 +1,11 @@
 package com.example.anmol.thirstquencher;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -13,6 +16,33 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     public void register(View view) {
+        String username = ((EditText) findViewById(R.id.registerUsername)).getText().toString();
+        String password = ((EditText) findViewById(R.id.registerPassword)).getText().toString();
 
+        if (username.equals("")) {
+            CharSequence text = "Enter Username!";
+            Toast emptyUsername = Toast.makeText(RegistrationActivity.this.getApplicationContext(), text, Toast.LENGTH_LONG);
+            emptyUsername.show();
+        } else if (MainActivity.userpasswords.containsKey(username)) {
+            CharSequence text = "Username Already Taken!";
+            Toast alreadyTaken = Toast.makeText(RegistrationActivity.this.getApplicationContext(), text, Toast.LENGTH_LONG);
+            alreadyTaken.show();
+        } else if (password.equals("")) {
+            CharSequence text = "Enter Password!";
+            Toast emptyPassword = Toast.makeText(RegistrationActivity.this.getApplicationContext(), text, Toast.LENGTH_LONG);
+            emptyPassword.show();
+        } else {
+            MainActivity.userpasswords.put(username, password);
+            CharSequence text = "Account Created";
+            Toast accountCreated = Toast.makeText(RegistrationActivity.this.getApplicationContext(), text, Toast.LENGTH_LONG);
+            accountCreated.show();
+            Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        }
+    }
+
+    public void cancelRegistration(View view) {
+        RegistrationActivity.this.finish();
     }
 }
