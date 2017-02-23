35858e270b7150a217cac703212b8c006aa41cf3
dhurvgarg <dhurvgarg@gmail.com>
2017-02-23 11:54:49 -0500

    Adding a model class

---
+++ ThirstQuencherIOS/ThirstQuencher/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  ThirstQuencher
+//
+//  Created by Dhurv Garg on 2/23/17.
+//  Copyright © 2017 Hunter. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    
+    let users: Array<Any>
+    
+}
