601275d0ff5cae4924967219650f7e23725f47c6
dhurvgarg <dhurvgarg@gmail.com>
2017-03-02 23:13:34 -0500

    Added initial implementation of sqlite

---
+++ ThirstQuencherIOS/ThirstQuencher/UsersDB.swift
@@ -0,0 +1,14 @@
+//
+//  UsersDB.swift
+//  ThirstQuencher
+//
+//  Created by Dhurv Garg on 3/2/17.
+//  Copyright © 2017 Hunter. All rights reserved.
+//
+
+import Foundation
+import sqlite3
+
+class UsersDB {
+    
+}
