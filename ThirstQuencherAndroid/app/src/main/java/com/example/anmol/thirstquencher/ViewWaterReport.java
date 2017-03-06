ef5d4a3959a90eb740dc890f552ce712b97162c8
tithiraval <tithi.raval@gatech.edu>
2017-03-06 16:43:50 -0500

    Added View Report Activity

--- ThirstQuencherAndroid/app/src/main/java/com/example/anmol/thirstquencher/ViewWaterReport.java
+++ ThirstQuencherAndroid/app/src/main/java/com/example/anmol/thirstquencher/ViewWaterReport.java
@@ -3,28 +3,46 @@ package com.example.anmol.thirstquencher;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.content.Intent;
+import android.view.View;
 
 import javax.xml.transform.Source;
 
 public class ViewWaterReport extends AppCompatActivity {
 
     private SourceReport report;
-    private TextView usernameText;
-    private TextView homeAddressText;
-    private TextView emailAddressText;
-    private TextView titleText;
-    private TextView accountTypeText;
+    private TextView dateTimeText;
+    private TextView reportNumberText;
+    private TextView reporterText;
+    private TextView locationText;
+    private TextView waterTypeText;
+    private TextView waterConditionText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_water_report);
-
         report = MainActivity.waterReports.get(getIntent().getIntExtra("WATER_REPORT_INDEX", 0));
-        usernameText = (TextView) findViewById(R.id.viewProfileUsername);
-        homeAddressText = (TextView) findViewById(R.id.viewProfileHomeAddress);
-        emailAddressText = (TextView) findViewById(R.id.viewProfileEmailAddress);
-        titleText = (TextView) findViewById(R.id.viewProfileTitle);
-        accountTypeText = (TextView) findViewById(R.id.viewProfileAccountType);
+        reportNumberText = (TextView) findViewById(R.id.viewWaterReportNumberTextField);
+        dateTimeText = (TextView) findViewById(R.id.viewWaterReportDateTextField);
+        reporterText = (TextView) findViewById(R.id.viewWaterReportUsernameTextField);
+        locationText = (TextView) findViewById(R.id.viewWaterReportLocationTextField);
+        waterTypeText = (TextView) findViewById(R.id.viewWaterReportTypeTextField);
+        waterConditionText = (TextView) findViewById(R.id.conditionOfWaterTextView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        reportNumberText.setText(report.getReportNumber());
+        dateTimeText.setText(report.getDate().toString());
+        reporterText.setText(report.getUsername());
+        locationText.setText(report.getLocation());
+        waterTypeText.setText(report.getType().toString());
+        waterConditionText.setText(report.getCondition().toString());
+    }
+
+    public void back(View view) {
+        ViewWaterReport.this.finish();
     }
 }
