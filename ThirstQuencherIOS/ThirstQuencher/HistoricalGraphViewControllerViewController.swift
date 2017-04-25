d0877dcbc7d7ac28e08971de06d4b6187f47767f
dhurvgarg <dhurvgarg@gmail.com>
2017-04-25 13:59:34 -0400

    Colors

--- ThirstQuencherIOS/ThirstQuencher/HistoricalGraphViewControllerViewController.swift
+++ ThirstQuencherIOS/ThirstQuencher/HistoricalGraphViewControllerViewController.swift
@@ -70,7 +70,6 @@ class HistoricalGraphViewControllerViewController: UIViewController {
         
         
         months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        let PPM = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
         
         setChart(dataPoints: months, virusPPM: virusPPM, contPPM: contaminantPPM)
         
@@ -91,10 +90,11 @@ class HistoricalGraphViewControllerViewController: UIViewController {
         }
         
         let virusChartDataSet = LineChartDataSet(values: virusDataEntries, label: "Virus PPM")
+        virusChartDataSet.setColor(UIColor.red)
+        virusChartDataSet.setCircleColor(UIColor.red)
         let contChartDataSet = LineChartDataSet(values: contDataEntries, label: "Contaminant PPM")
         let chartData = LineChartData(dataSets: [virusChartDataSet, contChartDataSet])
         lineChart.data = chartData
-        let xAxis = XAxis()
         lineChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: months)
         lineChart.xAxis.granularity = 1.0
     }
